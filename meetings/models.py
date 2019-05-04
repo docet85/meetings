64862dbf2c4f3e1140199d1ffdf52334a13fea72
FabioV <fabio.veronese@incaseofengineering.com>
2019-05-04 23:41:33 +0200

    drafted APIs

--- meetings/models.py
+++ meetings/models.py
@@ -1,14 +1,18 @@
 from datetime import datetime
 
-from meetings import db, app
+from meetings import db
+
+"""
+For sake of implementation these are persistency-centered models: they contain all the information but have no method 
+and perform no "action". All the behavior is coded inside so.called "routes", that act de-facto like controllers.
+"""
 
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     available = db.Column(db.Boolean, nullable=False, default=lambda: True)
 
-
-presence = db.Table('presence',
+inclusion = db.Table('inclusion',
                     db.Column('person_id', db.Integer, db.ForeignKey('person.id'), primary_key=True),
                     db.Column('meeting_id', db.Integer, db.ForeignKey('meeting.id'), primary_key=True)
                     )
@@ -16,30 +20,51 @@ presence = db.Table('presence',
 
 class Meeting(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    ts = db.Column(db.DateTime, nullable=False, default=datetime.now)
-    presenter = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
-    ongoing = db.Column(db.Boolean, nullable=False, default=lambda: True)
+    creation_ts = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    start_ts = db.Column(db.DateTime, nullable=True)
+    end_ts = db.Column(db.DateTime, nullable=True)
+    presenter_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
+    ongoing = db.Column(db.Boolean, nullable=False, default=lambda: False)
 
-    participants = db.relationship('Person', secondary=presence, lazy='subquery',
+    # relations are handy to retrieve the whole related object/set of related object without querying directly for them
+    presenter = db.relationship('Person', primaryjoin='Meeting.presenter_id == Person.id')
+    participants = db.relationship('Person', secondary=inclusion, lazy='subquery',
                                    backref=db.backref('meetings', lazy=True))
 
 
-class AvailabilityEvent(db.Model):
+class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ts = db.Column(db.DateTime, nullable=False, default=datetime.now)
     person_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
-    available = db.Column(db.Boolean, nullable=False, default=lambda: False)
+    meeting_id = db.Column(db.Integer, db.ForeignKey('meeting.id'), primary_key=True)
+    event_id = db.Column(db.Integer, nullable=False)
 
-    person = db.relationship('Person', backref=db.backref('person', lazy=True))
+    """  for simplicity and extendability no enum but int - there should be a static table with definitions - but for a
+         demo hardcoded values are enough.
+         1 - meeting start
+         2 - meeting stop
+         3 - person added
+         4 - person missing
+         5 - person available
+         6 - person added to meeting
+         7 - person removed from meeting
+         """
 
 
-def seed():
+def seed(n_users=10):
+    """
+    Convenience method generating users. Max 49 unique names are available.
+    :return:
+    """
     from random import randint, random
-    from time import sleep
+
+    if n_users > 49 or n_users <= 0:
+        raise ValueError("The number of users to seed should be greater than 0 and lower than 50")
+
     names = ['Jane', 'Jack', 'John', 'Jill', 'Jade', 'Jim', 'Jes']
     surnames = ['Doe', 'Dam', 'Der', 'Dew', 'Dat', 'Dow', 'Dyl']
     ppl = 0
-    while ppl < 10:
+    while ppl < n_users:
         name = names[randint(0, 6)] + ' ' + surnames[randint(0, 6)]
         if Person.query.filter_by(username=name).first():
             continue
@@ -47,5 +72,3 @@ def seed():
         db.session.add(new_user)
         db.session.commit()
         ppl += 1
-
-    db.session.flush()
--- meetings/routes.py
+++ meetings/routes.py
@@ -1,18 +1,76 @@
-from flask import render_template
+from flask import render_template, request, make_response
+from meetings.models import Meeting, Person, inclusion
+from meetings import db
 
 
 def create_routes(app):
 
     @app.route("/")
     def home():
-        from meetings.models import Meeting, Person
-        latest_meeting = Meeting.query.order_by(Meeting.ts.desc()).first()
+        """ This is the main page - here we select the persons and let the meeting start or stop it when
+        done.
+        """
+        ongoing = True
+        latest_meeting = Meeting.query.order_by(Meeting.start_ts.desc()).first()
         persons = Person.query.all()
         if latest_meeting is None or not latest_meeting.ongoing:
             ongoing = False
-        return render_template('home.jinja2', latest_meeting=latest_meeting, ongoing=ongoing, persons=persons)
+            ongoing_participants = [p.id for p in persons if p.available]
+        else:
+            ongoing_participants = [p.id for p in latest_meeting.participants]
+
+        return render_template('home.jinja2', latest_meeting=latest_meeting, ongoing=ongoing, persons=persons,
+                               ongoing_participants=ongoing_participants)
 
     @app.route("/hist")
     def hist():
-        from meetings.models import Meeting
+        """
+        The history page, displaying the last meetings, and the exclusion log
+        """
         return render_template('hist.jinja2', query_results=Meeting.query.paginate(per_page=5))
+
+    # ########################## API ############################ #
+
+    @app.route("/api/create_meeting", methods=['POST'])
+    def create_meeting():
+        m = Meeting()
+        m.participants = [p for p in Person.query.filter_by(available=True).all()]
+        db.session.add(m)
+        db.session.commit()
+        return make_response(200, 'ok')
+
+    @app.route("/api/start_meeting", methods=['POST'])
+    def start_meeting():
+        from datetime import datetime
+        m = Meeting.query.order_by(Meeting.start_ts.desc()).first()
+        m.start = datetime.now()
+        m.ongoing = True
+        # db.session.add(m)
+        db.session.commit()
+        return make_response(200, 'ok')
+
+    @app.route("/api/stop_meeting", methods=['POST'])
+    def stop_meeting():
+        from datetime import datetime
+        m = Meeting.query.order_by(Meeting.start_ts.desc()).first()
+        m.stop = datetime.now()
+        m.ongoing = False
+        # db.session.add(m)
+        db.session.commit()
+        return make_response(200, 'ok')
+
+    @app.route("/api/person", methods=['POST'])
+    def person():
+        j_person = request.json
+        p = Person(**j_person)
+        db.session.add(p)
+        db.session.commit()
+        return make_response(200, 'ok')
+
+    @app.route("/api/participant/<meeting_id>/<person_id>", methods=['POST', 'DELETE'])
+    def participant(meeting_id, person_id):
+        if request.method == 'POST':
+            inclusion.insert().values(person_id=person_id, meeting_id=meeting_id)
+        else:
+            inclusion.delete().where(person_id=person_id, meeting_id=meeting_id)
+        return make_response(200, 'ok')
